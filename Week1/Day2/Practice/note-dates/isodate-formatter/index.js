b3292217439fbe63b250d64da7b67b9ee955f77a
med-mokhtar-nasrali <mokhtarbackup03@gmail.com>
2024-11-27 17:29:21 +0100

    Fixing Node Dates

--- Week1/Day2/Practice/note-dates/isodate-formatter/index.js
+++ Week1/Day2/Practice/note-dates/isodate-formatter/index.js
@@ -1,3 +1,3 @@
 import {dateTimeYear, detailDay,dateBySlash} from "./utils/formatDate.js"
 
-console.log(dateTimeYear,dateBySlash,detailDay);
+console.log(dateTimeYear("11-27-2024-17-25Z"),dateBySlash("11/27/2024"),detailDay("Wednesday-November 17-2024"));
