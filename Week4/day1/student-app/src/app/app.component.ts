cc118407e7b8d58be9abf2df91d07c8ae23161c6
med-mokhtar-nasrali <mokhtarbackup03@gmail.com>
2024-12-18 14:53:54 +0100

    Angular Assignment 2

--- Week4/day1/student-app/src/app/app.component.ts
+++ Week4/day1/student-app/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StudentListComponent } from './student-list/student-list.component';
+import { Student } from './student';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StudentListComponent],
+  imports: [RouterOutlet, StudentListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Welcome to Student App!';
+  course = 'Full-Stack Development';
+  message = 'Student list has been updated!';
+
+  allStudents: Student[] = [];
+  onNotify(message: string) {
+    console.log(message);
+  }
 }
--- Week4/day1/student-app/src/app/student-list/student-list.component.ts
+++ Week4/day1/student-app/src/app/student-list/student-list.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Student } from '../student';
 
 @Component({
   selector: 'app-student-list',
@@ -8,6 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './student-list.component.css',
 })
 export class StudentListComponent {
+  update = 'Student list has been updated!';
+  @Input() course = '';
+  @Output() notifyParent = new EventEmitter<string>();
+
+  sendStudents() {
+    this.notifyParent.emit(this.update);
+  }
+
   students = [
     { id: 1, name: 'Alice', age: 22 },
     { id: 2, name: 'Bob', age: 24 },
